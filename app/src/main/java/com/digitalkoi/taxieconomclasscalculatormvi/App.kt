1918b5898cd225206d668ec7f795d4b76a6cbe89
digitalkoi <tarasfreedev@gmail.com>
2018-03-04 23:38:30 +0100

    added dependencies to gradle

---
+++ app/src/main/java/com/digitalkoi/taxieconomclasscalculatormvi/App.kt
@@ -0,0 +1,14 @@
+package com.digitalkoi.taxieconomclasscalculatormvi
+
+import android.app.Application
+
+/**
+ * @author Taras Zhupnyk (akka DigitalKoi) on 04/03/18.
+ */
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
